d01c09588919c8833efeecbaf1704cc0e4d0d842
Diana Mirlanbekova <diana.mirlanbekova@maibornwolff.de>
2023-12-05 13:54:29 +0100

    Stamm ist fertig, brauche noch --

--- Baum.java
+++ Baum.java
@@ -1,48 +1,80 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
-public class Baum{
-    public static void main (String[] args) {
-      try (Scanner sc = new Scanner(System.in)) {
-        System.out.print("Enter the height of tree: ");  
-        int height = sc.nextInt(); 
+public class Baum {
 
-        /* Defining: a=Rows, b=stars, c=spaces */ 
-        int a;
-        int b;
-        int c;
-        /*creating a loop for rows */
-            for(a=1; a<=height; a++){
-              for(c=1; c<=height-a; c++){
-                  System.out.print(" ");                
-                }
+  public static void main(String[] args) {
 
-            for(b=1; b<=a; b++){
-                System.out.print("*");
-              }
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Enter the height of tree: ");
+      int height = sc.nextInt();
 
-            for(b=2; b<=a; b++){
-                System.out.print("*");  
-            }
-          
-            System.out.println();
+      // Defining: a=Rows, b=stars, c=spaces
+      int a;
+      int b;
+      int c;
+      // creating a loop for rows
+      for (a = 1; a <= height; a++) {
 
-                /*Creating if statement for trunk of tree when height <= 5*/
-                if(height<5){ 
-                
-                  System.out.println("|");
-                  System.out.println("|");
-                  
-                }  
+        // if first line (a==1) drawTip()
+        // else draw leafRow() (stuff below)
+        for (c = 1; c <= height - a; c++) {
+          System.out.print(" ");
+        }
 
-                /* Creating if statement for trunk of tree when height <= 9*/
-                if(height<9 ){
-                  System.out.println("| |");
-                  System.out.print("| |");
-                }
-          
-            }
+
+
+        for (b = 1; b <= a; b++) {
+          System.out.print("*");
+        }
+
+        for (b = 2; b <= a; b++) {
+          System.out.print("*");
+        }
+
+        System.out.println();
+
+      }
+
+      // Creating if statement for trunk of tree when height <= 5
+      if (height <= 5) {
+        // Call space method
+        drawSpaces(height);
+        System.out.println("|");
+
+        // Call it again
+        drawSpaces(height);
+        System.out.println("|");
+
+      }
+
+      // Creating if statement for trunk of tree when height <= 9
+      if (height > 5) {
+
+        drawSpaces(height);
+        System.out.println("| |");
+        drawSpaces(height);
+        System.out.print("| |");
       }
 
     }
+
+  }
+
+  // Method that draws spaces depending on height
+
+  private static void drawSpaces(int height) {
+
+    if (height <= 5) {
+      for (int i = 0; i < height - 1; i++) {
+        System.out.print(" ");
+      }
+    } else {
+      for (int i = 0; i < height - 2; i++) {
+        System.out.print(" ");
+      }
+    }
+
+  }
+
 }
